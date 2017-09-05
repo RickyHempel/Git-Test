d87f325b449df169cfd196d14da860728804c7e4
riclhemp <riclhemp@ut.utm.edu>
2017-09-05 13:57:01 -0500

    Made to number guessing game!

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,34 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            int rnumber = rnd.Next(0,100);
+            int guess;
+            int tries=1;
+            bool loop_control = false;
+
+            Console.WriteLine("I am thinking of a number between 0 and 100! Can you guess the number.\n");
+          
+            do {
+                Console.Write("\nPlease enter your guess!\n");
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == rnumber)
+                {
+                    Console.WriteLine("\nHey you got this right the number was!The number of tries you took was:\n"+ tries);
+                    loop_control = true;
+                }
+                else if (guess > rnumber)
+                {
+                    Console.WriteLine("\nYour number is too high please try again.\n");
+
+                }
+                else if(guess < rnumber)
+                {
+                    Console.WriteLine("\nYour was too low please try again.\n");
+                }
+                tries++;
+            } while (!loop_control);
+            Console.WriteLine("\nPress any button to quit the program!\n");
             Console.ReadKey();
         }
     }
